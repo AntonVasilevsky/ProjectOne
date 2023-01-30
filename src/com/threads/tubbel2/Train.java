d40c40fff619cd8eaed6a17da9304ff1ef84bf73
Anton Vasilevsky <q3antonio@gmail.com>
2023-01-30 16:34:43 +0300

    Trains are moving though the tunnel one by one Trains switching tunnels after x time

--- src/com/threads/tubbel2/Train.java
+++ src/com/threads/tubbel2/Train.java
@@ -2,10 +2,14 @@ package com.threads.tubbel2;
 
 import com.threads.tunnel.Tunnel;
 
+import java.util.Date;
+
 public class Train implements Runnable{
     private static int counter = 1;
     private int id;
     private static Tunnel tunnel = new Tunnel();
+    private static Tunnel tunnel1 = new Tunnel();
+
 
     public Train() {
         id = counter;
@@ -26,15 +30,42 @@ public class Train implements Runnable{
         operate();
     }
     public  void operate(){
-        synchronized (tunnel) {
-            System.out.println("Train " + getId() + " starts moving in thread "
-                    + Thread.currentThread().getName());
-            try {
-                Thread.sleep(3000);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
+        long startTime = System.currentTimeMillis();
+        long elapsedTime = 0l;
+        while (elapsedTime<2000) {
+            elapsedTime = (new Date()).getTime() - startTime;
+            synchronized (tunnel) {
+                System.out.println("Train " + getId() + " North tunnel in thread "
+                        + Thread.currentThread().getName());
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                System.out.println("Train " + getId() + " has arrived");
             }
-            System.out.println("Train " + getId() + " has arrived");
         }
+        System.out.println("Train " + getId() + "switching a monitor");
+        operate1();
+
+    }
+    public  void operate1() {
+        long startTime = System.currentTimeMillis();
+        long elapsedTime = 0l;
+        while (elapsedTime < 2000) {
+            elapsedTime = (new Date()).getTime() - startTime;
+            synchronized (tunnel1) {
+                System.out.println("Train " + getId() + " South tunnel in thread "
+                        + Thread.currentThread().getName());
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                System.out.println("Train " + getId() + " has arrived");
+            }
+        }
+        System.out.println("Train " + getId() + "switching a monitor");
+        operate();
     }
 }
